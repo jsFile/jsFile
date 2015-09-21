b938f8b22b5b4579d5434495cc17873b44deef28
Denys <webschik@gmail.com>
2015-09-21 11:58:24 +0200

    Update buildStyle.js

--- src/document/src/html/src/buildStyle.js
+++ src/document/src/html/src/buildStyle.js
@@ -1,4 +1,19 @@
-export default function () {
+import toHyphenCase from './../../../../utils/toHyphenCase.js';
+
+export default function (styles) {
     const el = document.createElement('style');
+    const src = el.textContent = styles.map(({rules, selector}) => {
+        let declaration = '';
+        for (let prop in rules) {
+            if (rules.hasOwnProperty(prop)) {
+                declaration += `${toHyphenCase(prop)}: ${rules[prop]};\n`;    
+            }
+        }
+        
+        return `${selector} {
+            ${declaration}
+        }`;
+    }).join('\n');
+    el.appendChild(document.createTextNode(src));
     return el;
 }
